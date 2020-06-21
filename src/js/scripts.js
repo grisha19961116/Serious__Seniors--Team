3e5b965cdf18443da3322e3eec8b542a6018f703
Andriy Kikot <andray838@gmail.com>
2020-06-21 21:54:20 +0300

    send ajax form

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -15,3 +15,21 @@ $('form').validate({
     }
 });
 $('.contact__form-input--phone').mask("+38(999)999-9999", { autoclear: false });
+
+
+$(document).ready(function () {
+
+    $("#ajax_form").submit(function () {
+        $.ajax({
+            type: "POST",
+            url: "./form/telegram.php",
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find("input").val("");
+            alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
+            $("#ajax_form").trigger("reset");
+        });
+        return false;
+    });
+
+});
